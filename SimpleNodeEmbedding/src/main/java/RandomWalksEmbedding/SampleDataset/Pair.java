3212f0871ec5837d17840be7b597e2cbfb120136
Mohammed Al-Eiadeh <93108547+Mohammed-Ryiad-Eiadeh@users.noreply.github.com>
2026-04-02 05:54:32 -0400

    Delete SimpleNodeEmbedding directory

--- SimpleNodeEmbedding/src/main/java/RandomWalksEmbedding/SampleDataset/Pair.java
+++
@@ -1,7 +0,0 @@
-package RandomWalksEmbedding.SampleDataset;
-
-/**
- * Immutable value object representing a pair of vertices,
- * used to model (target, context) relationships.
- */
-public record Pair(int v1, int v2) { }
